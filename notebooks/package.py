11aa1ef63e41446ba030a5e07a5ef6ecdd71086e
hoangdaoviet <daoviethoang1402@gmail.com>
2024-12-27 15:21:20 +0700

    Reprocess data

--- notebooks/package.py
+++ notebooks/package.py
@@ -8,102 +8,111 @@ import numpy as np
 
 # Import deepcopy function
 from copy import deepcopy
+import datetime
 
-columns_to_drop = [
-    'released_year',
-    'released_month',
-    'released_day',
-    'in_spotify_playlists',
-    'in_spotify_charts',
-    'in_apple_playlists',
-    'in_apple_charts',
-    'in_deezer_playlists',
-    'in_deezer_charts',
-    'in_shazam_charts',
-    'instrumentalness_%',
-    'speechiness_%',
-    'liveness_%',
-]
+def clean_test_set(test_set, standard_scaler, corr_lambda):
+    # Drop unimportant columns
+    columns_to_drop = [
+        'in_spotify_playlists',
+        'in_spotify_charts',
+        'in_apple_playlists',
+        'in_apple_charts',
+        'in_deezer_playlists',
+        'in_deezer_charts',
+        'in_shazam_charts',
+        'instrumentalness_%'
+    ]
+    test_set = test_set.drop(columns=columns_to_drop)
 
-all_keys = ['c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'a', 'a#', 'b', 'unknown']
-unique_modes = ['major', 'minor']
+    # Remove unwanted characters from column names
+    test_set.columns = test_set.columns.str.replace('_', ' ')
+    test_set.columns = test_set.columns.str.replace('%', 'percentage')
 
-# KEY ENCODING
-# Assign indices to known keys
-key_to_index = {key: i for i, key in enumerate(all_keys[:-1])}
+    # Handle missing values
+    test_set['key'] = test_set['key'].fillna("unknown")
 
-# Encode function
-def encode_key(key):
-    if key == 'unknown':
-        return 0.0, 0.0, 1.0  # Unknown key encoding
-    index = key_to_index[key]
-    sine = np.sin(2 * np.pi * index / 12)
-    cosine = np.cos(2 * np.pi * index / 12)
-    return sine, cosine, 0.0  # Known key encoding
-
-# Encode all keys
-encoded_keys = {key: encode_key(key) for key in all_keys}
-
-# Convert to DataFrame
-keys_df = pd.DataFrame(encoded_keys, index=['sine', 'cosine', 'unknown']).sort_index().T
-
-columns_to_standard_scale = [
-    'bpm',
-    'danceability percentage',
-    'valence percentage',
-    'energy percentage',
-]
-
-columns_to_boxcox_transform = [
-    'streams',
-    'acousticness percentage',
-]
-
-def clean_test_data(
-        dataset: pd.DataFrame,
-        standard_scaler: StandardScaler,
-        corr_lmbda: list) -> pd.DataFrame:
-    # Drop unnecessary columns
-    dataset = dataset.drop(columns=columns_to_drop)
-    
-    # Remove the characters from the column names
-    dataset.columns = dataset.columns.str.replace('_', ' ')
-    dataset.columns = dataset.columns.str.replace('%', 'percentage')
-
-    # Address Inconsistencies
-    # For all string columns, standardize the text by converting them into lowercase
-    # and remove leading and trailing whitespaces
-    string_columns = dataset.select_dtypes(include=['object']).columns
-
+    # Standardize string columns
+    string_columns = test_set.select_dtypes(include=['object']).columns
     for col in string_columns:
-        dataset[col] = dataset[col].str.lower().str.strip()
+        test_set[col] = test_set[col].str.lower().str.strip()
+
+    # Encode 'mode' column
+    test_set['mode'] = (test_set['mode']
+                        .replace('unknown', 0)
+                        .replace('major', 1)
+                        .replace('minor', 2))
+
+    all_keys = ['c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'a', 'a#', 'b', 'unknown']
+    key_to_index = {key: i for i, key in enumerate(all_keys[:-1])}
+
+    # Encode 'key' column
+    def encode_key(key):
+        if key == 'unknown':
+            return 0.0, 0.0, 1.0  # Unknown key encoding
+        index = key_to_index[key]
+        sine = np.sin(2 * np.pi * index / 12)
+        cosine = np.cos(2 * np.pi * index / 12)
+        return sine, cosine, 0.0  # Known key encoding
     
-    # When cleaning the test set, on columns like `key` and `mode`, 
-    # if there's a value doesn't exist in the train set, we will replace it with `unknown`.
-    dataset['key'] = dataset['key'].fillna('unknown').apply(lambda x: 'unknown' if x not in all_keys else x)
-    dataset['mode'] = dataset['mode'].fillna('unknown').apply(lambda x: 'unknown' if x not in unique_modes else x)
-    dataset['mode'] = (dataset['mode']
-                    .replace('unknown', 0)
-                    .replace('major', 1)
-                    .replace('minor', 2))
-    dataset['key_sine'] = dataset['key'].apply(lambda x: encoded_keys[x][0])
-    dataset['key_cosine'] = dataset['key'].apply(lambda x: encoded_keys[x][1])
-    dataset['key_unknown'] = dataset['key'].apply(lambda x: encoded_keys[x][2])
-    dataset.drop(columns=['key'], inplace=True)
+    encoded_keys = {key: encode_key(key) for key in all_keys}
 
-    # Standardize the nearly normally distributed population columns
-    # This assumes that you have fit the standard scaler on the training set
-    temp_df = pd.DataFrame(standard_scaler
-                       .transform(dataset[columns_to_standard_scale]),
-                       columns = columns_to_standard_scale)
+    test_set['key sine'] = test_set['key'].apply(lambda x: encoded_keys[x][0])
+    test_set['key cosine'] = test_set['key'].apply(lambda x: encoded_keys[x][1])
+    test_set['key unknown'] = test_set['key'].apply(lambda x: encoded_keys[x][2])
+    test_set.drop(columns=['key'], inplace=True)
 
-    # We have to reset the index, so that when matching the index, the values are assigned correctly.
-    # If not, the values will be assigned based on the index of the original dataset, leading to some NaN values.
-    dataset.reset_index(drop=True, inplace=True) #optional because we don't remove outliers, thus not affecting the index
-    dataset[columns_to_standard_scale] = temp_df
+    # Create 'released day of week' column
+    def get_day_of_week(year, month, day):
+        return datetime.datetime(year, month, day).weekday()
 
-    # Apply Box-Cox transformation on right-skewed columns
+    test_set['released day of week'] = test_set.apply(
+        lambda row: get_day_of_week(row['released year'], row['released month'], row['released day']), 
+        axis=1
+        )
+
+    # Standard scale columns
+    columns_to_standard_scale = [
+        'bpm',
+        'danceability percentage',
+        'valence percentage',
+        'energy percentage'
+    ]
+    temp_df = pd.DataFrame(standard_scaler.transform(test_set[columns_to_standard_scale]), columns=columns_to_standard_scale)
+    test_set.reset_index(drop=True, inplace=True)
+    test_set[columns_to_standard_scale] = temp_df
+
+    # Apply Box-Cox transformation
+    columns_to_boxcox_transform = [
+        'streams',
+        'acousticness percentage',
+        'liveness percentage',
+        'speechiness percentage'
+    ]
     for col in columns_to_boxcox_transform:
-        dataset[col] = boxcox(dataset[col] + 1, lmbda=corr_lmbda[col])
+        test_set[col] = boxcox(test_set[col] + 1, lmbda=corr_lambda[col])
+
+    # Drop unnecessary columns
+    test_set = test_set.drop(columns=[
+        'bpm', 'energy percentage', 'acousticness percentage', 'liveness percentage', 
+        'released year', 'mode', 'key sine'
+        ])
+
+    return test_set
+
+def plot_histogram_and_boxplot(data, column):
+    """
+    This function plots a histogram and a boxplot for a numerical column in a dataset.
+
+    Parameters:
+    data (pandas.DataFrame): The dataset to be used.
+    column (str): The name of the numerical column to be plotted.
+
+    Returns:
+    None
+    """
+    fig, axs = plt.subplots(nrows=1, ncols=2, figsize=(15, 4))
     
-    return dataset
+    data[column].plot(kind='hist', ax=axs[0], title=column)
+    data[column].plot(kind='box', ax=axs[1], title=column, vert=False)
+
+    plt.tight_layout()
